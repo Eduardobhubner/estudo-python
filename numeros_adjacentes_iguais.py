6a92cc8d6e43aabf5577e8ea28be6f1d81ede76c
Eduardo Balan Hubner <eduardobhubner@gmail.com>
2021-02-18 16:02:38 -0300

    novos-exercicios

--- numeros_adjacentes_iguais.py
+++ numeros_adjacentes_iguais.py
@@ -1,17 +1,21 @@
-numero = int(input("Escreva um número inteiro para descobrir se há adjacentes iguais:"))
+numero = int(input("Digite um número inteiro:"))
 
 verificador = False
+contador =  len(str(numero))
 i = 0
 
-while not verificador and i < len(str(numero)):
-    count1 = numero % 10 
-    numero = numero // 10
-    count2 = numero % 10
-    if count1 == count2:
-        verificador = True
+if numero == 0:
+    print("não")
+else: 
+    while not verificador and i < contador:
+        count1 = numero % 10 
+        numero = numero // 10
+        count2 = numero % 10
+        if count1 == count2:
+            verificador = True
+        else:
+            i += 1
+    if verificador:
+        print("sim")
     else:
-        i += 1
-if verificador:
-    print("Existe")
-else:
-    print("Não existe")
+        print("não")
--- numeros_primos.py
+++ numeros_primos.py
@@ -1,2 +1,13 @@
+#numeros primos só podem ser divididos por ele e por 1 :)
 x = int(input("Digite um número inteiro:"))
+total = 0
+
+for c in range (1, x + 1):
+    if x % c == 0:
+        total += 1
+if total == 2:
+    print("primo")
+else:
+    print("não primo")
+
 
