a0c7a5cabe1352a6cb638a55e4c5f29aa8ad50e2
Eduardo Balan Hubner <eduardobhubner@gmail.com>
2021-03-04 23:07:29 -0300

    novo-exercicios

--- contaSegundo.py
+++ contaSegundo.py
@@ -6,6 +6,9 @@ h = int(rest_h // 3600)
 rest_m = rest_h % 3600
 m = int(rest_m // 60)
 rest_s = rest_m % 60
+
 segFinal = int(rest_s)
+
+
 
 print(dia,"dias,",h,"horas,",m,"minutos e",segFinal,"segundos.")
