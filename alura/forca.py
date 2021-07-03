00bba04e30af4bcf3717523c445f3a4daba7e8f0
Eduardo balan Hubner <eduardobhubner@gmail.com>
2021-07-03 17:39:35 -0300

    Manipulação de str e avançando nos estudos

--- alura/forca.py
+++ alura/forca.py
@@ -1,7 +1,35 @@
-import random
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da forca!***")
+    print("*********************************")
 
-print("*********************************")
-print("***Bem vindo ao jogo da forca!***")
-print("*********************************")
 
-print("Fim do jogo")
+
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+
+        chute = input("Selecione uma letra")
+        chute = chute.strip()
+        chute = chute.upper()
+
+        index = 0
+        for letra in palavra_secreta.upper():
+            if(letra == chute):
+                print("Você acertou, ela existe na posição {}".format(index))
+            index = index +1
+
+        print("Jogando...")
+
+    print("Fim do jogo")
+
+
+
+
+
+
+if __name__ == "__main__":
+    jogar()
--- alura/jogos.py
+++ alura/jogos.py
@@ -1,11 +1,20 @@
 import adivinhacao
 import forca
 
-print("*********************************")
-print("********Lista de jogos*********!")
-print("*********************************")
+def escolhe_jogo():
+    print("*********************************")
+    print("********Lista de jogos*********!")
+    print("*********************************")
 
-print("(1)Adivinhação, (2)Forca")
-jogo_escolhido = int(input("Escolha um dos jogos"))
+    print("(1)Adivinhação, (2)Forca")
+    jogo_escolhido = int(input("Escolha um dos jogos:"))
 
+    if jogo_escolhido == 1:
+        adivinhacao.jogar()
+    elif jogo_escolhido == 2:
+        forca.jogar()
+
+
+if __name__ == "__main__":
+    escolhe_jogo()
 
